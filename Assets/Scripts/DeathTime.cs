ec3e17f3af1c70071ba5ebafd4a1fb9449c098ba
DeLucaJ <delucaj96@gmail.com>
2018-11-10 20:09:25 -0800

    basic shooting

---
+++ Assets/Scripts/DeathTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTime : MonoBehaviour {
+
+    public float lifespan;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lifespan -= Time.deltaTime;
+
+        if (lifespan <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -8,6 +8,11 @@ public class Fire : MonoBehaviour {
     public float fireForce;
     public float coolDown = 0.2f;
 
+    //bullet stuff
+    public GameObject bulletPrefab;
+    public float spawnDist;
+    public float bulletForce;
+
     private float coolTime = 0.0f;
     private Rigidbody rb;
 	// Use this for initialization
@@ -19,10 +24,22 @@ public class Fire : MonoBehaviour {
     {
         if (Input.GetAxis(fireButton) > 0.0f && coolDown == coolTime)
         {
-            rb.AddForceAtPosition(transform.parent.forward * fireForce, transform.position, ForceMode.Impulse);
-            //rb.AddRelativeForce(transform.parent.forward * fireForce, ForceMode.Impulse);
-            coolTime = 0.0f;
+            FireBullet();
         }
+    }
+
+    public void FireBullet()
+    {
+        GameObject bullet = GameObject.Instantiate(
+                bulletPrefab,
+                transform.position - (transform.parent.forward * spawnDist),
+                transform.parent.rotation
+            );
+
+        bullet.GetComponent<Rigidbody>().AddForce(-1.0f * transform.forward * bulletForce, ForceMode.Impulse);
+
+        rb.AddForceAtPosition(transform.parent.forward * fireForce, transform.position, ForceMode.Impulse);
+        coolTime = 0.0f;
     }
 
     // Update is called once per frame
