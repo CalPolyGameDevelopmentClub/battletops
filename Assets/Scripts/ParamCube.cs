07ca7bf7b371623599f4544239b0f8e2433b11b9
Megan Washburn <memwashburn@gmail.com>
2018-11-12 01:13:02 -0800

    MUSIC VISUALIZATION

---
+++ Assets/Scripts/ParamCube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+    public int band;
+    public float startScale, scaleMult;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = new Vector3(transform.localScale.x, (AudioPeer._freqBand[band] * scaleMult) + startScale, transform.localScale.z);
+	}
+}
