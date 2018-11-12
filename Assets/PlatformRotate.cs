4dcd4c3b51a6d4b4f12c47370bfa391e95d97a38
DeLucaJ <delucaj96@gmail.com>
2018-11-12 11:50:47 -0800

    level changes

--- Assets/PlatformRotate.cs
+++ Assets/PlatformRotate.cs
@@ -8,13 +8,29 @@ public class PlatformRotate : MonoBehaviour {
     public int curY;
     public int deltY;
 
+    public float minH;
+    public float maxH;
+    public float speed;
+
+    private float toggle = 1.0f;
+
 	// Use this for initialization
 	void Start () {
         positions = new Vector2[4];
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+        transform.Translate(new Vector3(0.0f, toggle * speed, 0.0f));
+
+        if (transform.position.y <= minH)
+        {
+            toggle = 1.0f;
+        }
+        if (transform.position.y >= maxH)
+        {
+            toggle = -1.0f;
+        }
 	}
 }
