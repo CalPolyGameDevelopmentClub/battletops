6e94cc40aaf2587f7433a4124d04482cd0d595df
Megan Washburn <memwashburn@gmail.com>
2018-11-11 16:06:19 -0800

    Adding HUD to Level_01 scene, edited Health.cs to reflect in HUD

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
@@ -8,6 +10,40 @@ public class Health : MonoBehaviour {
     public int currentLives;
     public float maxHP;
     public float currentHP;
+
+    public Slider healthSlider;
+    public Image damageImage;
+    public Image[] stock; // 3 lives for now
+    public float flashSpeed = 5f;
+    public Color flashCol = new Color(1f, 0f, 0f, 0.1f); // Match to player color
+
+    AudioSource playerAudio;
+    public AudioClip damageClip, deathClip;
+
+    private void Awake()
+    {
+        playerAudio = GetComponent<AudioSource>();
+        currentHP = maxHP;
+
+        GameObject go = GameObject.Find("HUDCanvas");
+        if (!go)
+            return;
+        stock = go.GetComponents<Image>();
+
+        for (int i = 0; i < maxLives; i++)
+        {
+            stock[i].enabled = true;
+        }
+    }
+
+    //private void Update()
+    //{
+    //    if (currentHP < maxHP) { damageImage.color = flashCol; }
+    //    else
+    //    {
+    //        damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+    //    }
+    //}
 
     public void Damage(float damage)
     {
@@ -21,8 +57,15 @@ public class Health : MonoBehaviour {
 
     private void Death()
     {
+        //Respawn();
+
         currentLives--;
         currentHP = maxHP;
+
+        if (currentLives > maxLives) { stock[maxLives - currentLives].enabled = false; }
+        playerAudio.PlayOneShot(deathClip);
+        //Particles();
+
         if (currentLives <= 0) Destroy(gameObject);
     }
 }
